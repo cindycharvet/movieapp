7e6a3439d7f45b575e28716b6172381f254ddf94
cindycharvet <cindycharvetweb@gmail.com>
2024-02-01 01:17:50 -0800

    starting app tests

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Im Cindy, super coder</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MoviesScreen from './src/components/MoviesScreen';
+import SearchScreen from './src/components/searchScreen';
+import TVShowsScreen from './src/components/tvShowsScreen';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Movies" component={MoviesScreen} />
+        <Tab.Screen name="SearchResults" component={SearchScreen} />
+        <Tab.Screen name="TVShows" component={TVShowsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+<Tab.Screen
+  name="Movies"
+  component={MoviesScreen}
+  options={{
+    title: 'Movie App',
+    headerStyle: {
+      backgroundColor: '#f4511e', 
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }}
+/>
+
+export default App;
---
+++ src/components/tvShowsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TVShowsScreen = () => {
+  return (
+    <View>
+      <Text>TV Shows Screen</Text>
+    </View>
+  );
+};
+
+export default TVShowsScreen;
